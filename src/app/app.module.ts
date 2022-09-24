b1d005118692e40b58ceb72b23382796862c0bf9
Balla <ballaphanindra@outlook.com>
2022-09-24 18:41:31 +0530

    Committing Register Feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,20 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './Component/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { SigninComponent } from './Component/signin/signin.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    SigninComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
